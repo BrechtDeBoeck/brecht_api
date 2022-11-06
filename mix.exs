aec732c29f8643f20903285b442a23309e245a93
BrechtDeBoeck <r0838388@ucll.be>
2022-11-06 21:42:08 +0100

    prod config en env-vars

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Api_Brecht_De_BoeckR0838388.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:distillery, "~> 2.0", warn_missing: false}
     ]
   end
 
